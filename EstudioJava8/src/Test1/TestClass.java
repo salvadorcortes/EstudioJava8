1dab15a94f552b3d5cda217494e9c475d64d3f1c
salvador <salvador@ChavaPC>
2017-02-27 15:07:10 -0600

    Resultado

--- EstudioJava8/src/Test1/TestClass.java
+++ EstudioJava8/src/Test1/TestClass.java
@@ -7,7 +7,7 @@ public class TestClass{
       Stack s1 = new Stack ();
       Stack s2 = new Stack ();
       processStacks (s1,s2);
-      System.out.println (s1 + "    "+ s2);
+      System.out.println (s1 + "    "+ s2); //[100]    []
    }
    public static void processStacks(Stack x1, Stack x2){
       x1.push (new Integer ("100")); //assume that the method push adds the passed object to the stack.
